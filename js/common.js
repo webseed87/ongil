272302edd3d0b012d3b6df65b6b511665f202c85
webseed <thdwngusl@naver.com>
2023-10-12 23:04:12 +0900

    메인수정

--- js/common.js
+++ js/common.js
@@ -1,97 +1,128 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // 메인슬라이더
-const swiper = new Swiper(".swiper-container", {
+  // 메인슬라이더
+  const swiper = new Swiper(".swiper-container", {
     slidesPerView: 1,
     mousewheel: {
-        forceToAxis: true,
+      forceToAxis: true,
     },
     effect: 'fade',
     loop: true,
     autoplay: {
-        delay: 8000,
-        disableOnInteraction: false,
+      delay: 8000,
+      disableOnInteraction: false,
     },
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     }
-});
-// 사업분야 부분
-$(".field_item").on("mouseenter", function() {
+  });
+  var $videoElements = $('video');
+  var $loadingOverlays = $('.loading-overlay');
+
+  // 각 video 요소에 대한 이벤트 리스너를 추가합니다.
+  $videoElements.each(function(index) {
+    var $video = $(this);
+    var $loadingOverlay = $loadingOverlays.eq(index);
+
+    // 비디오의 loadeddata 이벤트를 사용하여 로딩 레이어를 숨깁니다.
+    $video.on('loadeddata', function() {
+      setTimeout(function() {
+        $loadingOverlay.css('display', 'none');
+      }, 1000); // 딜레이를 적절하게 조절하세요.
+    });
+  });
+
+  // 사업분야 부분
+  $(".field_item").on("mouseenter", function () {
     $(this).addClass("active");
     $(".field_item").not(this).removeClass("active");
-        var businessItem = "." + $(this).data("target");
-    $(businessItem).addClass("active"); // "actvie" 대신 "active"로 수정
+    var businessItem = "." + $(this).data("target");
+    $(businessItem).addClass("active"); 
 
     $(".business_item").not(businessItem).removeClass("active");
+  });
 });
 
-// 네비게이션 부분
-$(document).ready(function() {
+
+  // 네비게이션 부분
+  $(document).ready(function () {
+
+      checkScreenSize();
+
     
+    $(window).on("resize", function () {
+      checkScreenSize();
+    });
     function checkScreenSize() {
-        if ($(window).width() >= 1200) {
-            $(".navigation .menu").removeClass("active");
-          $(".menu").on("mouseenter", function() {
-            $(this).addClass("active");
-          });
-      
-          $(".menu").on("mouseleave", function() {
-            $(this).removeClass("active");
-          });
-                $(".menu").off("click");
-        } else {
-          $(".navigation .menu").on("click", function() {
-       
-                $(".navigation .menu.active").removeClass("active");
-                $(this).addClass("active");
+      if ($(window).width() >= 1200) {
+        $(".menu").off("click");
+        $(".navigation .menu").removeClass("active");
+        $(".menu").on("mouseenter", function () {
+          $(this).addClass("active");
+        });
 
-            
-          });
-           $(".menu").off("mouseenter");
-          $(".menu").off("mouseleave");
-        }
+        $(".menu").on("mouseleave", function () {
+          $(this).removeClass("active");
+        });
+        $(".menu").off("click");
+      } else {
+        $(".menu").off("mouseenter"); 
+        $(".menu").off("mouseleave"); 
+        $(".navigation .menu").on("click", function () {
+
+          $(".navigation .menu.active").removeClass("active");
+          $(this).addClass("active");
+
+
+        });
+     
       }
-      
-      // 페이지 로드 및 창 크기 변경 시 화면 크기 확인
-      $(document).ready(function() {
-        checkScreenSize();
-      });
-      
-      $(window).on("resize", function() {
-        checkScreenSize();
-      });
+    }
 
 
-    $(".menu-starter").on("click", function() {
-        $(".navigation .menu").eq(0).addClass("active");
-        if ($("#header").hasClass('menuing')) {
-            $("#header").removeClass('menuing')
+    $(".menu-starter").on("click", function () {
+      $(".navigation .menu").eq(0).addClass("active");
+      if ($("#header").hasClass('menuing')) {
+        $("#header").removeClass('menuing')
 
-        }
-        else{
-            $("#header").addClass('menuing');
-           
-        }
+      }
+      else {
+        $("#header").addClass('menuing');
+
+      }
 
     });
-    
-});
-});
+
+  });
 
 
-if (window.innerWidth > 720) {
-    $('#fullpage').fullpage({
+function checkScreenSize() {
+  if (window.innerWidth > 720) {
+    if (typeof fullpage_api === 'undefined') {
+      $('#fullpage').fullpage({
         navigation: false,
         fixedElements: '#header',
         normalScrollElements: '.footer',
-    });
+      });
+    }
   } else {
-    fullpage_api.destroy('all');
+    if (typeof fullpage_api !== 'undefined') {
+      fullpage_api.destroy('all');
+    }
   }
+}
+
+// 페이지 로드 시와 창 크기 변경 시 화면 크기 확인
+$(document).ready(function () {
+  checkScreenSize();
+});
+
+$(window).on("resize", function () {
+  checkScreenSize();
+});
